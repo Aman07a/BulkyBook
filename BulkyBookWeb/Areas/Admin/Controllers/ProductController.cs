14a81256248ce263604ecefe6712908f475e6931
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-05 14:27:53 +0100

    Projections using Select

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections;
 using System.Linq;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
@@ -26,6 +28,18 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 		public IActionResult Upsert(int? id)
 		{
 			Product product = new();
+
+			IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+			{
+				Text = u.Name,
+				Value = u.Id.ToString()
+			});
+
+			IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(u => new SelectListItem
+			{
+				Text = u.Name,
+				Value = u.Id.ToString()
+			});
 
 			if (id == null || id == 0)
 			{
