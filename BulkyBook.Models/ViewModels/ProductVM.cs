52a0fb0bc92130db0e005ed7c731676a21e78644
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-06 08:14:36 +0100

    Create First Product

--- BulkyBook.Models/ViewModels/ProductVM.cs
+++ BulkyBook.Models/ViewModels/ProductVM.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,7 +12,11 @@ namespace BulkyBook.Models.ViewModels
 	public class ProductVM
 	{
 		public Product Product { get; set; }
+
+		[ValidateNever]
 		public IEnumerable<SelectListItem> CategoryList { get; set; }
+
+		[ValidateNever]
 		public IEnumerable<SelectListItem> CoverTypeList { get; set; }
 	}
 }
