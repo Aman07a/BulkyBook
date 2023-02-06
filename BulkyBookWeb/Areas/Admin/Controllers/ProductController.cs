b9523441f38dee601783283d5a96abbdb3a0bd87
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-06 10:08:16 +0100

    Delete API Endpoint

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -111,42 +111,6 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 			return View(obj);
 		}
 
-		// GET
-		public IActionResult Delete(int? id)
-		{
-			Product product = new();
-
-			if (id == null || id == 0)
-			{
-				// Create product
-				return View(product);
-			}
-			else
-			{
-				// Delete product
-			}
-
-			return View();
-		}
-
-		// POST
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public IActionResult DeletePOST(int? id)
-		{
-			var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
-
-			if (obj == null)
-			{
-				return NotFound();
-			}
-
-			_unitOfWork.Product.Remove(obj);
-			_unitOfWork.Save();
-			TempData["success"] = "Product deleted successfully";
-			return RedirectToAction("Index");
-		}
-
 		#region API CALLS
 
 		[HttpGet]
@@ -154,6 +118,29 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 		{
 			var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
 			return Json(new { data = productList });
+		}
+
+		// POST
+		[HttpDelete]
+		public IActionResult Delete(int? id)
+		{
+			var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+
+			if (obj == null)
+			{
+				return Json(new { success = false, message = "Error while deleting" });
+			}
+
+			var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+
+			if (System.IO.File.Exists(oldImagePath))
+			{
+				System.IO.File.Delete(oldImagePath);
+			}
+
+			_unitOfWork.Product.Remove(obj);
+			_unitOfWork.Save();
+			return Json(new { success = true, message = "Delete Successful" });
 		}
 
 		#endregion
