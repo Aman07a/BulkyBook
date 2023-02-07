0826d20b6b85577a7ca5e5ae5c2a5ceb7d06e513
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-07 16:35:01 +0100

    Order Confirmation

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -170,7 +170,23 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 		{
 			OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == id);
 
+			var service = new SessionService();
+			Session session = service.Get(orderHeader.SessionId);
+
 			// Check the stripe status
+			if (session.PaymentStatus.ToLower() == "paid")
+			{
+				_unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+				_unitOfWork.Save();
+			}
+
+			List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == 
+				orderHeader.ApplicationUserId).ToList();
+
+			_unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+			_unitOfWork.Save();
+
+			return View(id);
 		}
 
 		public IActionResult Plus(int cartId)
