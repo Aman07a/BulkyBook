d95a52f7a8696ab03c927e0702fa2bdb3b1e6040
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-07 11:34:13 +0100

    Price calculation based on quantity

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -29,7 +29,29 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 				includeProperties: "Product")
 			};
 
+			foreach (var cart in ShoppingCartVM.ListCart)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, 
+					cart.Product.Price50, cart.Product.Price100);
+			}
+
 			return View(ShoppingCartVM);
+		}
+
+		private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
+		{
+			if (quantity <= 50)
+			{
+				return price;
+			}
+			else
+			{
+				if (quantity <= 100)
+				{
+					return price50;
+				}
+				return price100;
+			}
 		}
 	}
 }
