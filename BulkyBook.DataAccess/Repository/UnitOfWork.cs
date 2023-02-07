51b180cdb8d8c9c789f7a08e661eb677830655a4
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-07 13:46:33 +0100

    Add Order Header and Detail to UnitOfWork

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -29,6 +29,8 @@ namespace BulkyBook.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
         public void Save()
         {
