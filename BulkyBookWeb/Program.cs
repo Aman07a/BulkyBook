4f64b5e3093e3e1dfc9b7974d3ec53ae23b6fd54
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-09 14:05:10 +0100

    Implement DbInitializer

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using BulkyBook.Models;
 using Stripe;
 using System;
+using BulkyBook.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,7 @@ builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Str
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
 	.AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddAuthentication().AddFacebook(options =>
@@ -66,6 +68,7 @@ app.UseRouting();
 
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
+SeedDatabase();
 app.UseAuthentication();
 
 app.UseAuthorization();
@@ -77,3 +80,12 @@ app.MapControllerRoute(
 	pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+		dbInitializer.Initialize();
+	}
+}
