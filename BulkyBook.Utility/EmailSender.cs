8e1d537b505553e9b1c4b88ccca67c1b3af31659
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-09 11:05:17 +0100

    SendGrid

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,6 +1,9 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
+using SendGrid.Helpers.Mail;
+using SendGrid;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +14,13 @@ namespace BulkyBook.Utility
 {
 	public class EmailSender : IEmailSender
 	{
+		// public string SendGridSecret { get; set; }
+
+		// public EmailSender(IConfiguration _config)
+		// {
+		// 	SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+		// }
+
 		public Task SendEmailAsync(string email, string subject, string htmlMessage)
 		{
 			// var emailToSend = new MimeMessage();
@@ -30,6 +40,13 @@ namespace BulkyBook.Utility
 			// }
 
 			return Task.CompletedTask;
+
+			// var client = new SendGridClient(SendGridSecret);
+			// var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
+			// var to = new EmailAddress(email);
+			// var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+			// return client.SendEmailAsync(msg);
 		}
 	}
 }
